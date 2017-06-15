d76f4c6837c21383fc4076bca2d3cc2a66a0cda9
Alda Marteau-Hardi <alda@leetchee.fr>
2017-06-15 07:20:31 +0200

    Change entrypoint to go around a docker bug

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,7 +8,8 @@ echo "Creating mastodon user (UID : ${UID} and GID : ${GID})..."
 addgroup -g ${GID} mastodon && adduser -h /mastodon -s /bin/sh -D -G mastodon -u ${UID} mastodon
 
 echo "Updating permissions..."
-find /mastodon -path /mastodon/public/system -prune -o -not -user mastodon -not -group mastodon -print0 | xargs -0 chown -f mastodon:mastodon
+#find /mastodon -path /mastodon/public/system -prune -o -not -user mastodon -not -group mastodon -print0 | xargs -0 chown -f mastodon:mastodon
+chown -Rf mastodon:mastodon /mastodon
 
 echo "Executing process..."
 exec su-exec mastodon:mastodon /sbin/tini -- "$@"
